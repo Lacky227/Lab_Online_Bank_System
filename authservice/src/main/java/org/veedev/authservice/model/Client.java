c70e358605894448b930cf41d7164f650dba9654
Lacky227 <tishko.artyr@gmail.com>
2025-03-30 12:31:58 +0300

    feat(model-Client): created model Client

---
+++ authservice/src/main/java/org/veedev/authservice/model/Client.java
@@ -0,0 +1,28 @@
+package org.veedev.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String password;
+}
