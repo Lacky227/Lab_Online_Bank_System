91f7b6ca51f00242f5241a6a2d925e3086b8de0c
Lacky227 <tishko.artyr@gmail.com>
2025-03-30 22:41:40 +0300

    feat(util-AccountUtil): created AccountUtil for generating random account numbers and pin codes

---
+++ accountservice/src/main/java/org/veedev/accountservice/util/AccountUtil.java
@@ -0,0 +1,24 @@
+package org.veedev.accountservice.util;
+
+import lombok.experimental.UtilityClass;
+import java.util.Random;
+
+@UtilityClass
+public class AccountUtil {
+    private final Random random = new Random();
+
+    private static String generateAccountNumber() {
+        StringBuilder accountNumber = new StringBuilder();
+        for (int i = 0; i < 16; i++) {
+            accountNumber.append(random.nextInt(10));
+        }
+        return accountNumber.toString();
+    }
+    public static String generatePinCode(){
+        StringBuilder pinCodeBuilder = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            pinCodeBuilder.append(random.nextInt(10));
+        }
+        return pinCodeBuilder.toString();
+    }
+}
