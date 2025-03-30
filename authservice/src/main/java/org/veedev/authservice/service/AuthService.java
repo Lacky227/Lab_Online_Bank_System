00c4de2b11e1c7bea3b9df8f871b087c617ab8e3
Lacky227 <tishko.artyr@gmail.com>
2025-03-30 12:41:28 +0300

    feat(service-AuthService): created AuthService and added methods register, login

---
+++ authservice/src/main/java/org/veedev/authservice/service/AuthService.java
@@ -0,0 +1,10 @@
+package org.veedev.authservice.service;
+
+import org.springframework.http.ResponseEntity;
+import org.veedev.authservice.dto.LoginRequest;
+import org.veedev.authservice.dto.RegisterRequest;
+
+public interface AuthService {
+    ResponseEntity<String> register(RegisterRequest registerRequest);
+    ResponseEntity<String> login(LoginRequest loginRequest);
+}
