63d718aa724ce986138898e9d6389d67d340b2e0
Lacky227 <tishko.artyr@gmail.com>
2025-03-30 13:08:09 +0300

    feat(repository-AuthRepository): created AuthRepository and added methods for to check if a client exists by email, phone number, and to find a user by phone number

---
+++ authservice/src/main/java/org/veedev/authservice/repository/AuthRepository.java
@@ -0,0 +1,12 @@
+package org.veedev.authservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.veedev.authservice.model.Client;
+
+@Repository
+public interface AuthRepository extends JpaRepository<Client, Long> {
+    Boolean existsByEmail(String email);
+    Boolean existsByPhoneNumber(String phoneNumber);
+    Client findByPhoneNumber(String phoneNumber);
+}
