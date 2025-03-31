3e2068a6a105d617e182671b3afcc62bf6d239f0
Lacky227 <tishko.artyr@gmail.com>
2025-03-31 16:14:45 +0300

    feat(model-Transaction, TransactionType): create Transaction model and enum class for transaction type

---
+++ transactionservice/src/main/java/org/veedev/transactionservice/model/Transaction.java
@@ -0,0 +1,18 @@
+package org.veedev.transactionservice.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Currency;
+
+@Data
+public class Transaction {
+    private String transactionId;
+    private String numberAccount;
+    private BigDecimal amount;
+    private Currency currency;
+    private TransactionType transactionType;
+    private LocalDateTime timestamp;
+}
---
+++ transactionservice/src/main/java/org/veedev/transactionservice/model/TransactionType.java
@@ -0,0 +1,5 @@
+package org.veedev.transactionservice.model;
+
+public enum TransactionType {
+    DEPOSIT, WITHDRAW, TRANSFER
+}
