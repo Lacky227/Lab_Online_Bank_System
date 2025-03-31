b3ed723e108f2a5710387945becdd1b55366c000
Lacky227 <tishko.artyr@gmail.com>
2025-03-31 18:03:08 +0300

    feat(service-TransactionService, impl-TransactionServiceImpl): rename param to event and extend deposit and withdraw operations in TransactionServiceImpl with Kafka-based

--- transactionservice/src/main/java/org/veedev/transactionservice/impl/TransactionServiceImpl.java
+++ transactionservice/src/main/java/org/veedev/transactionservice/impl/TransactionServiceImpl.java
@@ -1,25 +1,35 @@
 package org.veedev.transactionservice.impl;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.veedev.transactionservice.dto.TransactionRequest;
 import org.veedev.transactionservice.dto.TransferRequest;
+import org.veedev.transactionservice.model.TransactionType;
 import org.veedev.transactionservice.service.TransactionService;
 
 @Service
+@AllArgsConstructor
 public class TransactionServiceImpl implements TransactionService {
+    private final KafkaTemplate<String, TransactionRequest> kafkaTemplate;
     @Override
-    public ResponseEntity<String> deposit(TransactionRequest transactionRequest) {
-        return null;
+    public ResponseEntity<String> deposit(TransactionRequest event) {
+        event.setTransactionType(TransactionType.DEPOSIT);
+        kafkaTemplate.send("transaction-requested", event);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @Override
-    public ResponseEntity<String> withdraw(TransactionRequest transactionRequest) {
-        return null;
+    public ResponseEntity<String> withdraw(TransactionRequest event) {
+        event.setTransactionType(TransactionType.WITHDRAW);
+        kafkaTemplate.send("transaction-requested", event);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @Override
-    public ResponseEntity<String> transfer(TransferRequest transferRequest) {
+    public ResponseEntity<String> transfer(TransferRequest event) {
         return null;
     }
 }
--- transactionservice/src/main/java/org/veedev/transactionservice/service/TransactionService.java
+++ transactionservice/src/main/java/org/veedev/transactionservice/service/TransactionService.java
@@ -5,7 +5,7 @@ import org.veedev.transactionservice.dto.TransactionRequest;
 import org.veedev.transactionservice.dto.TransferRequest;
 
 public interface TransactionService {
-    ResponseEntity<String> deposit(TransactionRequest transactionRequest);
-    ResponseEntity<String> withdraw(TransactionRequest transactionRequest);
-    ResponseEntity<String> transfer(TransferRequest transferRequest);
+    ResponseEntity<String> deposit(TransactionRequest event);
+    ResponseEntity<String> withdraw(TransactionRequest event);
+    ResponseEntity<String> transfer(TransferRequest event);
 }
