571a2e2b0e11e4a1a6ae001c3d72672770f222a4
Lacky227 <tishko.artyr@gmail.com>
2025-03-31 17:10:50 +0300

    feat(controller-TransactionController): created TransactionController for stateless authentication with /trans endpoints

---
+++ transactionservice/src/main/java/org/veedev/transactionservice/controller/TransactionController.java
@@ -0,0 +1,29 @@
+package org.veedev.transactionservice.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.veedev.transactionservice.dto.TransactionRequest;
+import org.veedev.transactionservice.dto.TransferRequest;
+import org.veedev.transactionservice.service.TransactionService;
+
+@RestController
+@RequestMapping("/trans")
+@AllArgsConstructor
+@CrossOrigin
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    @PostMapping("/deposit")
+    public ResponseEntity<String> deposit(@RequestBody TransactionRequest transactionRequest) {
+       return transactionService.deposit(transactionRequest);
+    }
+    @PostMapping("/withdraw")
+    public ResponseEntity<String> withdraw(@RequestBody TransactionRequest transactionRequest) {
+        return transactionService.withdraw(transactionRequest);
+    }
+    @PostMapping("/transfer")
+    public ResponseEntity<String> transfer(@RequestBody TransferRequest transactionRequest) {
+        return transactionService.transfer(transactionRequest);
+    }
+}
