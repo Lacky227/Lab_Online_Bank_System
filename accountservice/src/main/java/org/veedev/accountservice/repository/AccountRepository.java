3a49021e4466ddd65492f95cb689dd96c5de575d
Lacky227 <tishko.artyr@gmail.com>
2025-03-31 22:19:07 +0300

    feat(repository-AccountRepository): add method find account by number

--- accountservice/src/main/java/org/veedev/accountservice/repository/AccountRepository.java
+++ accountservice/src/main/java/org/veedev/accountservice/repository/AccountRepository.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Repository;
 import org.veedev.accountservice.model.Account;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findByClientId(Long clientId);
+    Account findByNumber(String number);
 }
