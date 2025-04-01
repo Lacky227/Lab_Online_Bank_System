ee758125f28ff86dd473839b5c2e845c82f20297
Lacky227 <tishko.artyr@gmail.com>
2025-04-01 23:30:13 +0300

    feat(service-ReportService): refactor saveTransaction method and add method for generate report

--- reportservice/src/main/java/org/veedev/reportservice/service/ReportService.java
+++ reportservice/src/main/java/org/veedev/reportservice/service/ReportService.java
@@ -1,9 +1,9 @@
 package org.veedev.reportservice.service;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.veedev.reportservice.dto.ReportDTO;
 import org.veedev.reportservice.model.Transaction;
 
 public interface ReportService {
-    void saveTransaction(ReportDTO reportDTO);
+    void saveTransaction(Transaction transaction);
+    void generateReport(ReportDTO reportDTO);
 }
