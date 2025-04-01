cf0cd6e1ca5d02290e75fe4f3e01fae77c2de321
Lacky227 <tishko.artyr@gmail.com>
2025-04-01 20:55:58 +0300

    feat(dto-TransactionRequest): add phoneNumber

--- transactionservice/src/main/java/org/veedev/transactionservice/dto/TransactionRequest.java
+++ transactionservice/src/main/java/org/veedev/transactionservice/dto/TransactionRequest.java
@@ -8,6 +8,7 @@ import java.math.BigDecimal;
 @Data
 public class TransactionRequest {
     private String accountNumber;
+    private String phoneNumber;
     private BigDecimal amount;
     private TransactionType transactionType;
 }
