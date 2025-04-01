158f14f0c66c93ab9ba36cceb2c1d1e1b6a413d0
Lacky227 <tishko.artyr@gmail.com>
2025-04-01 20:53:00 +0300

    feat(service-AccountService, repository-AccountRepository): add methods updateAccount in AccountService and find account by number in AccountRepository

--- accountservice/src/main/java/org/veedev/accountservice/repository/AccountRepository.java
+++ accountservice/src/main/java/org/veedev/accountservice/repository/AccountRepository.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Repository;
 import org.veedev.accountservice.model.Account;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findByClientId(Long clientId);
+    Optional<Account> findByNumber(String number);
 }
--- accountservice/src/main/java/org/veedev/accountservice/service/AccountService.java
+++ accountservice/src/main/java/org/veedev/accountservice/service/AccountService.java
@@ -2,8 +2,11 @@ package org.veedev.accountservice.service;
 
 import org.springframework.http.ResponseEntity;
 import org.veedev.accountservice.dto.CreateAccountRequest;
+import org.veedev.accountservice.dto.TransactionRequest;
 import org.veedev.accountservice.model.Account;
+import org.veedev.accountservice.model.Transaction;
 
 public interface AccountService {
     ResponseEntity<String> createAccount(CreateAccountRequest request);
+    Transaction updateAccount(TransactionRequest request);
 }
