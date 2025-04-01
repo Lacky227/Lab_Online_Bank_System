f79d6d765dbffe5e7bb1b11e510246ec1a875ac4
Lacky227 <tishko.artyr@gmail.com>
2025-04-01 22:46:16 +0300

    feat(dto-ReportDTO): add ReportDTO for transaction and response data in reporting

---
+++ reportservice/src/main/java/org/veedev/reportservice/dto/ReportDTO.java
@@ -0,0 +1,12 @@
+package org.veedev.reportservice.dto;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.Data;
+import org.veedev.reportservice.model.Transaction;
+
+@Data
+public class ReportDTO {
+    private HttpServletResponse response;
+    private Transaction transaction;
+}
