1a7a9649450daf36687192d70429d8c0264d613b
Lacky227 <tishko.artyr@gmail.com>
2025-04-02 00:52:54 +0300

    refactor(controller-AccountController): refactor name endpoint to /update-balance

--- accountservice/src/main/java/org/veedev/accountservice/controller/AccountController.java
+++ accountservice/src/main/java/org/veedev/accountservice/controller/AccountController.java
@@ -1,9 +1,12 @@
 package org.veedev.accountservice.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.veedev.accountservice.dto.CreateAccountRequest;
+import org.veedev.accountservice.dto.TransactionRequest;
+import org.veedev.accountservice.model.Transaction;
 import org.veedev.accountservice.service.AccountService;
 
 @RestController
@@ -16,5 +19,9 @@ public class AccountController {
     @PostMapping("/create")
     ResponseEntity<String> create(@RequestBody CreateAccountRequest createAccountRequest){
         return accountService.createAccount(createAccountRequest);
+    }
+    @PostMapping("/update-balance")
+    Transaction updateBalance(@RequestBody TransactionRequest transactionRequest){
+        return accountService.updateAccount(transactionRequest);
     }
 }
