5df74fb73e4e8f7f1f3dff41b3c574f56545d496
Lacky227 <tishko.artyr@gmail.com>
2025-04-01 20:54:12 +0300

    feat(impl-AccountServiceImpl): implement method updateAccount and add logic

--- accountservice/src/main/java/org/veedev/accountservice/impl/AccountServiceImpl.java
+++ accountservice/src/main/java/org/veedev/accountservice/impl/AccountServiceImpl.java
@@ -6,8 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.veedev.accountservice.dto.CreateAccountRequest;
+import org.veedev.accountservice.dto.TransactionRequest;
 import org.veedev.accountservice.model.Account;
+import org.veedev.accountservice.model.Transaction;
+import org.veedev.accountservice.model.TransactionType;
 import org.veedev.accountservice.repository.AccountRepository;
 import org.veedev.accountservice.service.AccountService;
 import org.veedev.accountservice.util.AccountUtil;
@@ -15,6 +19,9 @@ import org.veedev.accountservice.util.AccountUtil;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -22,6 +29,7 @@ public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
     private final PasswordEncoder passwordEncoder;
     private final RedisTemplate<String, String> redisTemplate;
+    private final RestTemplate restTemplate;
     @Override
     public ResponseEntity<String> createAccount(CreateAccountRequest request) {
         String cacheKey = "session id:" + request.getPhoneNumber();
@@ -48,5 +56,32 @@ public class AccountServiceImpl implements AccountService {
         account.setEndAt(LocalDateTime.now().plusYears(5));
         accountRepository.save(account);
         return ResponseEntity.status(HttpStatus.CREATED).body("Account created successfully, your account number is: " + account.getNumber() + ", pin code is: " + pinCode + ", and account end is: " + account.getEndAt());
+    }
+
+    @Override
+    public Transaction updateAccount(TransactionRequest request) {
+        Optional<Account> account = accountRepository.findByNumber(request.getAccountNumber());
+        if (account.isEmpty()) {
+            throw new RuntimeException("Account not found");
+        } else if (Objects.equals(request.getTransactionType(), TransactionType.DEPOSIT)) {
+            account.get().setBalance(account.get().getBalance().add(request.getAmount()));
+        } else if (Objects.equals(request.getTransactionType(), TransactionType.WITHDRAW)) {
+            account.get().setBalance(account.get().getBalance().subtract(request.getAmount()));
+        }
+        accountRepository.save(account.get());
+        String cacheLastName = "session lastName:" + request.getPhoneNumber();
+        String cacheValueLastName = redisTemplate.opsForValue().get(cacheLastName);
+        if (cacheValueLastName == null) {
+            throw new RuntimeException("Account last name not found");
+        }
+        Transaction transaction = new Transaction();
+        transaction.setTransactionId(UUID.randomUUID().toString());
+        transaction.setLastName(cacheValueLastName);
+        transaction.setNumberAccount(account.get().getNumber());
+        transaction.setAmount(request.getAmount());
+        transaction.setCurrency(account.get().getCurrency().toString());
+        transaction.setTransactionType(request.getTransactionType());
+        transaction.setTimestamp(LocalDateTime.now());
+        return transaction;
     }
 }
