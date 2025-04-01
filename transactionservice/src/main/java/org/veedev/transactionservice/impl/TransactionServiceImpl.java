9b37844b8d442702f4ac16b7f9e2113f1f950999
Lacky227 <tishko.artyr@gmail.com>
2025-04-01 20:59:09 +0300

    feat(impl-TransactionServiceImpl): implements TransactionService and add logic for methods

--- transactionservice/src/main/java/org/veedev/transactionservice/impl/TransactionServiceImpl.java
+++ transactionservice/src/main/java/org/veedev/transactionservice/impl/TransactionServiceImpl.java
@@ -1,25 +1,28 @@
 package org.veedev.transactionservice.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.veedev.transactionservice.dto.TransactionRequest;
 import org.veedev.transactionservice.dto.TransferRequest;
+import org.veedev.transactionservice.service.RestService;
 import org.veedev.transactionservice.service.TransactionService;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionServiceImpl implements TransactionService {
+    private final RestService restService;
+
     @Override
     public ResponseEntity<String> deposit(TransactionRequest transactionRequest) {
-        return null;
+        restService.processTransaction(transactionRequest);
+        return ResponseEntity.status(HttpStatus.OK).body("Transaction deposited successfully");
     }
 
     @Override
     public ResponseEntity<String> withdraw(TransactionRequest transactionRequest) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<String> transfer(TransferRequest transferRequest) {
-        return null;
+        restService.processTransaction(transactionRequest);
+        return ResponseEntity.status(HttpStatus.OK).body("Transaction withdrawn successfully");
     }
 }
