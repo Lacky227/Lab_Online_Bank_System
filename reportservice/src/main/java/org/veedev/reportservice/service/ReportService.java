64cec83e5e37549a3ca919ab07c6db733c881431
Lacky227 <tishko.artyr@gmail.com>
2025-04-06 20:43:55 +0300

    feat(service-ReportService): add method for get Transaction by account number

--- reportservice/src/main/java/org/veedev/reportservice/service/ReportService.java
+++ reportservice/src/main/java/org/veedev/reportservice/service/ReportService.java
@@ -3,7 +3,10 @@ package org.veedev.reportservice.service;
 import jakarta.servlet.http.HttpServletResponse;
 import org.veedev.reportservice.model.Transaction;
 
+import java.util.List;
+
 public interface ReportService {
     void saveTransaction(Transaction transaction);
     void generateReport(String accountNumber, HttpServletResponse response);
+    List<Transaction> getTransactions(String accountNumber);
 }
