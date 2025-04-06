a415aa2aad88bc9fffc171222d96436ef08b4cc3
Lacky227 <tishko.artyr@gmail.com>
2025-04-06 11:10:38 +0300

    feat(repository-ClientRepository): create ClientRepository and add method get Client by phone number

---
+++ authservice/src/main/java/org/veedev/authservice/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package org.veedev.authservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.veedev.authservice.model.Client;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> getClientByPhoneNumber(String phoneNumber);
+}
