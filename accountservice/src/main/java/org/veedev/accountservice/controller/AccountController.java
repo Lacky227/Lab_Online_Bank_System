cfcfc38ec8e23cceabbb7ab23627f2611471479d
Lacky227 <tishko.artyr@gmail.com>
2025-04-06 15:05:53 +0300

    refactor(controller-AccountController): remove import for HttpStatus

--- accountservice/src/main/java/org/veedev/accountservice/controller/AccountController.java
+++ accountservice/src/main/java/org/veedev/accountservice/controller/AccountController.java
@@ -1,7 +1,6 @@
 package org.veedev.accountservice.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.veedev.accountservice.dto.ClientIdRequest;
