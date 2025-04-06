b7a03a12ebb6c734d8533d85d5bcc401e92fd44e
Lacky227 <tishko.artyr@gmail.com>
2025-04-06 20:40:35 +0300

    feat(controller-AccountController, dto-NumberRequest): add NumberRequest DTO

--- accountservice/src/main/java/org/veedev/accountservice/controller/AccountController.java
+++ accountservice/src/main/java/org/veedev/accountservice/controller/AccountController.java
@@ -3,10 +3,7 @@ package org.veedev.accountservice.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.veedev.accountservice.dto.AccountDetailsRequest;
-import org.veedev.accountservice.dto.ClientIdRequest;
-import org.veedev.accountservice.dto.CreateAccountRequest;
-import org.veedev.accountservice.dto.TransactionRequest;
+import org.veedev.accountservice.dto.*;
 import org.veedev.accountservice.model.Account;
 import org.veedev.accountservice.model.Transaction;
 import org.veedev.accountservice.service.AccountService;
@@ -33,7 +30,7 @@ public class AccountController {
         return accountService.getAccountsByClientId(clientIdRequest);
     }
     @GetMapping("/details")
-    AccountDetailsRequest getAccountDetails(@RequestBody String number){
-        return accountService.getAccountDetails(number);
+    AccountDetailsRequest getAccountDetails(@RequestBody NumberRequest numberRequest){
+        return accountService.getAccountDetails(numberRequest.getNumber());
     }
 }
---
+++ accountservice/src/main/java/org/veedev/accountservice/dto/NumberRequest.java
@@ -0,0 +1,8 @@
+package org.veedev.accountservice.dto;
+
+import lombok.Data;
+
+@Data
+public class NumberRequest {
+    private String number;
+}
