01a62599595465000b38a758807e18ff00cf9bf3
Lacky227 <tishko.artyr@gmail.com>
2025-04-06 15:03:53 +0300

    feat(controller-AccountController): add GET-method for get Accounts

--- accountservice/src/main/java/org/veedev/accountservice/controller/AccountController.java
+++ accountservice/src/main/java/org/veedev/accountservice/controller/AccountController.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.veedev.accountservice.dto.ClientIdRequest;
 import org.veedev.accountservice.dto.CreateAccountRequest;
 import org.veedev.accountservice.dto.TransactionRequest;
+import org.veedev.accountservice.model.Account;
 import org.veedev.accountservice.model.Transaction;
 import org.veedev.accountservice.service.AccountService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/account")
@@ -23,5 +27,9 @@ public class AccountController {
     @PostMapping("/update-balance")
     Transaction updateBalance(@RequestBody TransactionRequest transactionRequest){
         return accountService.updateAccount(transactionRequest);
+    }
+    @GetMapping("/list")
+    List<Account> getAccounts(@RequestBody ClientIdRequest clientIdRequest){
+        return accountService.getAccountsByClientId(clientIdRequest);
     }
 }
