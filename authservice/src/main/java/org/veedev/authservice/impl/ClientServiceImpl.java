9a79638fe616afb94ba33c5dbe68020f448ee59e
Lacky227 <tishko.artyr@gmail.com>
2025-04-06 11:18:32 +0300

    feat(impl-ClientServiceImpl): implement ClientService for get client by phone number

---
+++ authservice/src/main/java/org/veedev/authservice/impl/ClientServiceImpl.java
@@ -0,0 +1,24 @@
+package org.veedev.authservice.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.veedev.authservice.model.Client;
+import org.veedev.authservice.repository.ClientRepository;
+import org.veedev.authservice.service.ClientService;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ClientServiceImpl implements ClientService {
+    private final ClientRepository clientRepository;
+
+    @Override
+    public Client getClient(String phoneNumber) {
+        Optional<Client> client = clientRepository.getClientByPhoneNumber(phoneNumber);
+        if (client.isEmpty()) {
+            throw new RuntimeException("No client found with phone number " + phoneNumber);
+        }
+        return client.get();
+    }
+}
