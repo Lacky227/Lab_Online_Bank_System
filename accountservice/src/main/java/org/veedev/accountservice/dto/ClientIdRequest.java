1d3994032defa078a339e71b7981eebdc6f075c7
Lacky227 <tishko.artyr@gmail.com>
2025-04-06 14:56:48 +0300

    feat(service-AccountService, dto-ClientIdRequest): add method for get accounts by client id and create ClientIdRequest DTO

---
+++ accountservice/src/main/java/org/veedev/accountservice/dto/ClientIdRequest.java
@@ -0,0 +1,8 @@
+package org.veedev.accountservice.dto;
+
+import lombok.Data;
+
+@Data
+public class ClientIdRequest {
+    private Long clientId;
+}
--- accountservice/src/main/java/org/veedev/accountservice/service/AccountService.java
+++ accountservice/src/main/java/org/veedev/accountservice/service/AccountService.java
@@ -1,12 +1,16 @@
 package org.veedev.accountservice.service;
 
 import org.springframework.http.ResponseEntity;
+import org.veedev.accountservice.dto.ClientIdRequest;
 import org.veedev.accountservice.dto.CreateAccountRequest;
 import org.veedev.accountservice.dto.TransactionRequest;
 import org.veedev.accountservice.model.Account;
 import org.veedev.accountservice.model.Transaction;
 
+import java.util.List;
+
 public interface AccountService {
     ResponseEntity<String> createAccount(CreateAccountRequest request);
     Transaction updateAccount(TransactionRequest request);
+    List<Account> getAccountsByClientId(ClientIdRequest request);
 }
