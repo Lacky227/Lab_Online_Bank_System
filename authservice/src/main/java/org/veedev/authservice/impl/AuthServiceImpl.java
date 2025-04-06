3d60f858635bd1e4383707a129abd1d4d54441c1
Lacky227 <tishko.artyr@gmail.com>
2025-04-06 12:03:46 +0300

    feat(repository, impl-AuthServiceImpl): remove ClientRepository and rename AuthRepository to ClientRepository and add method for get Client by phone number

--- authservice/src/main/java/org/veedev/authservice/impl/AuthServiceImpl.java
+++ authservice/src/main/java/org/veedev/authservice/impl/AuthServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.veedev.authservice.dto.LoginRequest;
 import org.veedev.authservice.dto.RegisterRequest;
 import org.veedev.authservice.model.Client;
-import org.veedev.authservice.repository.AuthRepository;
+import org.veedev.authservice.repository.ClientRepository;
 import org.veedev.authservice.service.AuthService;
 
 import java.util.concurrent.TimeUnit;
@@ -17,14 +17,14 @@ import java.util.concurrent.TimeUnit;
 @Service
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
-    private final AuthRepository authRepository;
+    private final ClientRepository clientRepository;
     private final PasswordEncoder passwordEncoder;
     private final RedisTemplate<String, String> redisTemplate;
     @Override
     public ResponseEntity<String> register(RegisterRequest registerRequest) {
-        if (authRepository.existsByEmail(registerRequest.getEmail())) {
+        if (clientRepository.existsByEmail(registerRequest.getEmail())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already exists");
-        } else if (authRepository.existsByPhoneNumber(registerRequest.getPhoneNumber())) {
+        } else if (clientRepository.existsByPhoneNumber(registerRequest.getPhoneNumber())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Phone number already exists");
         }
         Client client = new Client();
@@ -33,13 +33,13 @@ public class AuthServiceImpl implements AuthService {
         client.setEmail(registerRequest.getEmail());
         client.setPhoneNumber(registerRequest.getPhoneNumber());
         client.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
-        authRepository.save(client);
+        clientRepository.save(client);
         return ResponseEntity.status(HttpStatus.CREATED).body("Client created");
     }
 
     @Override
     public ResponseEntity<String> login(LoginRequest loginRequest) {
-        Client client = authRepository.findByPhoneNumber(loginRequest.getPhoneNumber());
+        Client client = clientRepository.findByPhoneNumber(loginRequest.getPhoneNumber());
         if (client == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found with this phone number: " + loginRequest.getPhoneNumber());
         } else if (!passwordEncoder.matches(loginRequest.getPassword(), client.getPassword())) {
