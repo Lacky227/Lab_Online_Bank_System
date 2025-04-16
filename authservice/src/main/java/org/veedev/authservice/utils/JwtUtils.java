d44f17be017a0b95fcd394e9ba8720ce35fb161a
Lacky227 <tishko.artyr@gmail.com>
2025-04-16 23:15:34 +0300

    feat:(utils-JwtUtils): add JwtUtils component for JWT generation and phone number extraction

---
+++ authservice/src/main/java/org/veedev/authservice/utils/JwtUtils.java
@@ -0,0 +1,31 @@
+package org.veedev.authservice.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private final String secret = "secretKey";
+    private final long expiration = 3600000;
+
+    public String generationToken(String phoneNumber) {
+        return Jwts.builder()
+                .setSubject(phoneNumber)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS512)
+                .compact();
+    }
+    public String extractPhoneNumber(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
