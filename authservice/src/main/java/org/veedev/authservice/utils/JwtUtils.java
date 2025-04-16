26ad78708e01b3161d6d4a9a3255dbcd0dae7c6f
Lacky227 <tishko.artyr@gmail.com>
2025-04-16 23:16:55 +0300

    feat:(utils-JwtUtils): add validation token

--- authservice/src/main/java/org/veedev/authservice/utils/JwtUtils.java
+++ authservice/src/main/java/org/veedev/authservice/utils/JwtUtils.java
@@ -1,5 +1,6 @@
 package org.veedev.authservice.utils;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -27,5 +28,14 @@ public class JwtUtils {
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secret.getBytes()).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
     }
 }
