0070723c6daf49507441688743c6dc13c6721e0c
Lacky227 <tishko.artyr@gmail.com>
2025-04-23 23:21:30 +0300

    refactor:(config-GatewayConfig): add constructor injection, split auth_service routes, remove /auth from protected paths

--- apigateway/src/main/java/org/veedev/apigateway/config/GatewayConfig.java
+++ apigateway/src/main/java/org/veedev/apigateway/config/GatewayConfig.java
@@ -8,16 +8,26 @@ import org.veedev.apigateway.filter.JwtAuthenticationFilter;
 
 @Configuration
 public class GatewayConfig {
-    private JwtAuthenticationFilter jwtFilter;
+    private final JwtAuthenticationFilter jwtFilter;
 
+    public GatewayConfig(JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
     @Bean
     RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("auth_service", r -> r.path("/auth/**", "/profile").filters(f -> f.filter(jwtFilter)).uri("http://localhost:8081/"))
-                .route("account_service", r -> r.path("/account/**").filters(f -> f.filter(jwtFilter)).uri("http://localhost:8082/"))
+                .route("auth_service", r -> r.path("/profile")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("http://localhost:8081/"))
+                .route("auth_service", r ->
+                        r.path("/auth/login", "/auth/register")
+                                .uri("http://localhost:8081/"))
+                .route("account_service", r -> r
+                        .path("/account/**")
+                        .filters(f -> f.filter(jwtFilter))
+                        .uri("http://localhost:8082/"))
                 .route("transaction_service", r -> r.path("/trans/**").filters(f -> f.filter(jwtFilter)).uri("http://localhost:8083/"))
                 .route("report_service", r -> r.path("/report/download", "/report/transactions").filters(f -> f.filter(jwtFilter)).uri("http://localhost:8084/"))
                 .build();
     }
-
 }
