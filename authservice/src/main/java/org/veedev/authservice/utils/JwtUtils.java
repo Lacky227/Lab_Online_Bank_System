af1ebc445566d37ab8e1daafe8720a04ae500d68
Lacky227 <tishko.artyr@gmail.com>
2025-04-23 23:30:16 +0300

    refactor:(utils-JwtUtils): update secret to Base64-encoded, modify token generation, remove validation methods

--- authservice/src/main/java/org/veedev/authservice/utils/JwtUtils.java
+++ authservice/src/main/java/org/veedev/authservice/utils/JwtUtils.java
@@ -1,6 +1,5 @@
 package org.veedev.authservice.utils;
 
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -10,32 +9,15 @@ import java.util.Date;
 
 @Component
 public class JwtUtils {
-    private final String secret = "secretKey";
+    private final String secret = "VGhpcyBpcyBhIHZlcnkgc2VjdXJlIHNlY3JldCBrZXkgdGhhdCBoYXMgYWxsb3Qgb2YgYnl0ZXMgdGhhdCBhcmUgbmV2ZXIgcmV2ZWFsZWQh";
     private final long expiration = 3600000;
 
     public String generationToken(String phoneNumber) {
         return Jwts.builder()
-                .setSubject(phoneNumber)
+                .claim("phoneNumber", phoneNumber)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS512)
                 .compact();
-    }
-    public String extractPhoneNumber(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(secret.getBytes())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parserBuilder().setSigningKey(secret.getBytes()).build().parseClaimsJws(token);
-            return true;
-        } catch (JwtException | IllegalArgumentException e) {
-            return false;
-        }
     }
 }
