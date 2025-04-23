c8162889387b31ceab889cfd942ce35c62cbb3a0
Lacky227 <tishko.artyr@gmail.com>
2025-04-23 23:43:37 +0300

    feat:(utils-JwtUtils): modify phoneNumber extraction, add clientId extraction

--- apigateway/src/main/java/org/veedev/apigateway/utils/JwtUtils.java
+++ apigateway/src/main/java/org/veedev/apigateway/utils/JwtUtils.java
@@ -1,5 +1,6 @@
 package org.veedev.apigateway.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
@@ -9,12 +10,21 @@ public class JwtUtils {
     private final String secret = "VGhpcyBpcyBhIHZlcnkgc2VjdXJlIHNlY3JldCBrZXkgdGhhdCBoYXMgYWxsb3Qgb2YgYnl0ZXMgdGhhdCBhcmUgbmV2ZXIgcmV2ZWFsZWQh";
 
     public String extractPhoneNumber(String token) {
-        return Jwts.parserBuilder()
+        Claims claims = Jwts.parserBuilder()
                 .setSigningKey(secret.getBytes())
                 .build()
                 .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+                .getBody();
+        return claims.get("phoneNumber", String.class);
+    }
+
+    public Long extractClientId(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get("clientId", Long.class);
     }
 
     public boolean validateToken(String token) {
